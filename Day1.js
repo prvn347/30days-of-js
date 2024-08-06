ebb11aeca27c3918013148d1e702815836643e8c
Your New Username <sahupravin960@gmail.com>
2024-08-06 08:26:03 +0530

    day1

---
+++ Day1.js
@@ -0,0 +1,32 @@
+//var declaration
+var a = 10;
+console.log(a);
+//let declaration
+let b = "pravin";
+console.log(b);
+//constant declartion.
+const c = "8";
+console.log(c);
+
+//activity 3 data types
+let d = true;
+console.log(typeof d);
+let e = {
+  pravin: 3,
+  slfj: 3,
+};
+console.log(typeof e);
+const f = [2, 3, 3, 3];
+console.log(typeof f);
+
+//activity 4
+let g = 8;
+console.log(g);
+g = "pravin";
+console.log(g);
+
+//activity 5
+// const h = "ljasf";
+// console.log(h);
+// h = 8;
+// console.log(h);
